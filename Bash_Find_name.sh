601b67c2b78de8448052aae64dc388b87f5de209
IT-Support-L2 <67242173+IT-Support-L2@users.noreply.github.com>
2020-07-28 22:22:55 +0200

    Update Bash_Find_name.sh

--- Bash_Find_name.sh
+++ Bash_Find_name.sh
@@ -1,7 +1,11 @@
-> oldFiles.txt
-files=`grep jane* ~/learning/text.txt`
+> matched_pattern.txt  
+files=`grep jane* ~/directory/log.txt`
 for i in $files; do
- if [ -f ~/learning/$i ]; then
-     echo $i >> oldFiles.txt
+ if [ -f ~/directory/$i ]; then
+     echo $i >> matched_pattern.txt
 fi
 done
+
+<<COMMENTS
+This script grep 'jane' username using regex from a log file and append the matched pattern to matched_pattern.txt
+COMMENTS
