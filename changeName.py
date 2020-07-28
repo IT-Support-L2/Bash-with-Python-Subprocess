4d04a79697dfa516a590200bf347bb3a57fa6975
IT-Support-L2 <67242173+IT-Support-L2@users.noreply.github.com>
2020-07-28 22:33:07 +0200

    Update changeName.py

--- changeName.py
+++ changeName.py
@@ -8,3 +8,10 @@ with open(sys.argv[1], 'r') as f:
     res = dict(zip(content2, content3))
     for key, value in res.items():
         subprocess.run(['mv', '/home/ninja-coder/'+key, '/home/ninja-coder/'+value])
+        
+<<COMMENTS
+This Python script reads sys.avg[1] or the argument == file which will be executed with it. On Linux OS, command to run is  ./changeName.py matched_pattern.txt
+The use of subprocess module is a must since it's requested in the mentioned assessment.
+Finally, this script will substitute the old files usernames with the new ones after searching for it in the given directory.
+The hard part is that the pattern must match a part of the file name and change it. As an example of a file name to change: dxc98jane_rpt.doc >> dxc98jdoe_rpt.doc
+COMMENTS
